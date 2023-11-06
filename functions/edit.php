c87c709e13953c970b5c013dba3d53f74d8f84ed
charlynelorde <elordecharlyn74@gmail.com>
2023-11-06 20:05:53 +0800

    edit

--- functions/edit.php
+++ functions/edit.php
@@ -1,23 +1,23 @@
 <?php
-// including the database connection file
-include_once("../dbConnection/mysql.config_connection.php");
-if(isset($_POST['update'])) {
-$id $_POST['id'];
-$code $_POST['code'];
-$name=$_POST['name'];
-// checking empty fields
-if(empty($code) | empty($name)){
-if(empty($code)) {
-echo "<font color='red'>Subject Code field is empty.</font><br/>";
-}
-if(empty($name)) {
-echo "<font color='red'>Subject Name field is empty.</font><br/>";
-}
-}
-else {
-//updating the table
-mysqli_query($dbc, "UPDATE tblsubject SET subject code='$code', subject_name=$name HERE subject_id='$id'as");
-//redirectig to the display page. In our case, it is index.php header("Location: ../index.php");
-}
-}
+    // including the database connection file
+    include_once("../dbConnection/mysql.config_connection.php");
+    if(isset($_POST['update'])) {
+        $id $_POST['id'];
+        $code $_POST['code'];
+        $name=$_POST['name'];
+        // checking empty fields
+        if(empty($code) | empty($name)){
+            if(empty($code)) {
+                echo "<font color='red'>Subject Code field is empty.</font><br/>";
+            }
+            if(empty($name)) {
+                echo "<font color='red'>Subject Name field is empty.</font><br/>";
+            }
+        }
+        else {
+            //updating the table
+            mysqli_query($dbc, "UPDATE tblsubject SET subject code='$code', subject_name=$name HERE subject_id='$id'as");
+            //redirectig to the display page. In our case, it is index.php header("Location: ../index.php");
+        }
+    }
 ?>
