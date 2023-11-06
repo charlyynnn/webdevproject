0cf8a2ca9ce47862a1385e7ddba9e028723686c1
charlynelorde <elordecharlyn74@gmail.com>
2023-11-06 20:25:49 +0800

    modify

--- functions/add.php
+++ functions/add.php
@@ -13,6 +13,7 @@
         if (empty($code) || empty($name)) {
             if (empty($code)) {
                 echo "<font color='red'>Subject Code field is empty.</font><br/>";
+            }
                 if (empty($name)) {
                     echo "<font color='red'>Subject Name field is empty.</font><br/>";
                 }
