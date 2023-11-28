ced9b181af0a791d28a7193c10ff36aee9364613
charlynelorde <elordecharlyn74@gmail.com>
2023-11-28 17:28:30 +0800

    update

--- functions/fetch.php
+++ functions/fetch.php
@@ -1,7 +1,7 @@
 
 <?php
      include_once("../dbConnection/mysqlconfig_connection.php");
-    $query = "SELECT * FROM tblsubjects";
-    $result = mysqli_query($dbc, $query);
-    // using mysqli_query
+     SELECT * FROM tblsubjects
+     LEFT JOIN tblsyllabus
+     ON tblsyllabus.subject_id = tblsubjects.subjectid
  ?>
--- functions/index.php
+++ functions/index.php
@@ -17,6 +17,9 @@
                 <td>ID</td>
                 <td> Subject Code</td>
                 <td>Subject Name</td>
+                <td>Syllabus ID</td>
+                <td>Syllabus Code</td>
+                <td>Syllabus Author</td>
                 <td>Action</td>
             </tr>
 <?php
@@ -25,6 +28,9 @@
         echo "<td>".$res['Subject_ID']."</td>";
         echo "<td>".$res['Subject_Code']."</td>";
         echo "<td>".$res['Subject_Name']."</td>";
+        echo "<td>".$res['syllabus_id']."</td>";
+        echo "<td>".$res['syllabus_code']."</td>";
+        echo "<td>".$res['syllabus_author']."</td>";
         echo "<td><a href=\"../forms/editform.php?id=$res[Subject_ID]\">Edit</a> | 
         <a href=\"delete.php?id=$res[Subject_ID]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
     }
