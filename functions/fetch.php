f31b47f184c29f7aef627fdf2e9bd5b278088d3d
charlynelorde <elordecharlyn74@gmail.com>
2023-11-28 21:00:44 +0800

    update

--- functions/fetch.php
+++ functions/fetch.php
@@ -4,6 +4,6 @@
      $query=
      "SELECT * FROM tblsubjects
      LEFT JOIN tblsyllabus
-     ON tblsyllabus.subject_id = tblsubjects.subject_id";
+     ON tblsyllabus.subject_id = tblsubjects.Subject_ID";
      $result = mysqli_query ($dbc, $query);
  ?>
